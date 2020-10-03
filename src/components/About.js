e28dc7d4d0471790b68f9db405ba7628ea1aaacf
github username <github email address>
2020-10-03 13:07:43 -0400

    link added

--- src/components/About.js
+++ src/components/About.js
@@ -14,6 +14,8 @@ class About extends React.Component {
         of New York and a M.M. in Scoring for Film and Multimedia from New York University. After years of being a music teacher and performing with the band&nbsp;
         <a href="http://www.symfinity.com">Symfinity</a> which he founded, he enrolled at The Flatiron School and is now an active web developer experienced with Ruby, Rails, Javascript and React.js.
         He is also still active as a composer and a musician.</p>
+        <h2>This Site</h2>
+        <p>This site is built with React.js frontend and a Python/Flask backend. For hosting I use Netlify and domain provider I use <a href="https://namecheap.pxf.io/LvY6O">Namecheap</a>.</p>
       </div>
     )
   }
