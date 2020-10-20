0e6e7b5ca6c7d8e757e990db5440580db1d9c733
github username <github email address>
2020-10-20 10:47:49 -0400

    remove console logs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,7 +15,6 @@ class Contact extends React.Component {
 
   changeHandler = (e) => {
     this.setState({[e.target.name]: e.target.value})
-    console.log(e.target.value)
   }
 
   submitHandler = (e) => {
@@ -37,7 +36,7 @@ class Contact extends React.Component {
         message: message
       })
     }).then(res => res.json())
-    .then(res => {console.log(res, "console")})
+    .then(res => {})
   }
 
   render(){
